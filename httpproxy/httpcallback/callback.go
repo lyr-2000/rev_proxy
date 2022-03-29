6938cfd2b23da78c6d108c8d9152f7c4b2de5603
idea-your-father <1957476540@qq.com>
2022-03-29 13:35:07 +0800

    bugfix:redirect 301 bug

--- httpproxy/httpcallback/callback.go
+++ httpproxy/httpcallback/callback.go
@@ -14,11 +14,18 @@ func OnError(w http.ResponseWriter, r *http.Request, err error) {
 //修改回调
 
 func ModifyResponse(r *http.Response) error {
-	if r.StatusCode == 200 {
-		//log.Printf("请求成功")
+	//if r.StatusCode == 301 ||
+	//	r.StatusCode == 302 {
+	//	r.StatusCode = 200
+	//	//禁止被重定向到在线域名
+	//	r.Status = "200 OK\n"
+	//}
+	//if r.StatusCode == 200 {
+	//	//log.Printf("请求成功")
+	//
+	//}
+	//log.Printf("http status code = %v,  status = %v", r.StatusCode, r.Status)
 
-	}
-	
 	//log.Printf("conn = %s\n", r.Header.Get("Connection"))
 	//判断状态码，用来记录统计
 	return nil
--- httpproxy/middleware/rewrite.go
+++ httpproxy/middleware/rewrite.go
@@ -77,20 +77,23 @@ func customTransport(to *url.URL) http.RoundTripper {
 	}
 
 	var transport http.RoundTripper = &http.Transport{
-		//Proxy: nil, // http.ProxyFromEnvironment,
+		Proxy: http.ProxyFromEnvironment,
 		DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
+			//network == tcp and  addr == 443
+			//log.Printf("dial context %v, %v", network, addr)
 			return dialer.DialContext(ctx, network, addr)
 		},
 		MaxIdleConns:          8,
 		IdleConnTimeout:       90 * time.Second,
 		TLSHandshakeTimeout:   10 * time.Second,
-		ExpectContinueTimeout: 1 * time.Second,
+		ExpectContinueTimeout: 10 * time.Second,
 		DisableKeepAlives:     false,
 		//DialTLSContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
 		//	conn, err := dialTLS(network, addr)
 		//	return conn, err
 		//},
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true}, //跳过 https验证
+
 		//DisableCompression: true,
 		//dialt,
 
@@ -101,18 +104,76 @@ func customTransport(to *url.URL) http.RoundTripper {
 	//}
 	return transport
 }
+
+func singleJoiningSlash(a, b string) string {
+	aslash := strings.HasSuffix(a, "/")
+	bslash := strings.HasPrefix(b, "/")
+	switch {
+	case aslash && bslash:
+		return a + b[1:]
+	case !aslash && !bslash:
+		return a + "/" + b
+	}
+	return a + b
+}
+func joinURLPath(a, b *url.URL) (path, rawpath string) {
+	if a.RawPath == "" && b.RawPath == "" {
+		return singleJoiningSlash(a.Path, b.Path), ""
+	}
+	// Same as singleJoiningSlash, but uses EscapedPath to determine
+	// whether a slash should be added
+	apath := a.EscapedPath()
+	bpath := b.EscapedPath()
+
+	aslash := strings.HasSuffix(apath, "/")
+	bslash := strings.HasPrefix(bpath, "/")
+
+	switch {
+	case aslash && bslash:
+		return a.Path + b.Path[1:], apath + bpath[1:]
+	case !aslash && !bslash:
+		return a.Path + "/" + b.Path, apath + "/" + bpath
+	}
+	return a.Path + b.Path, apath + bpath
+}
 func NewUrlRewriteMiddleWare(from, to *url.URL) *RewriteUrlMiddleWare {
 
 	var transport = customTransport(to)
+	var (
+		old, now = strutil.String2Bytes(&from.Path), strutil.String2Bytes(&to.Path)
+	)
+	targetQuery := to.RawQuery
+	director := func(req *http.Request) {
+		//禁用重定向
 
-	revProxy := httputil.NewSingleHostReverseProxy(to)
+		raw := strutil.String2Bytes(&req.URL.Path)
+		req.URL.Scheme = to.Scheme
+		req.URL.Host = to.Host
+
+		req.URL.Path = strutil.Byte2String(strutil.UnsafeReplaceBegin(raw, old, now))
+		req.URL.Path, req.URL.RawPath = joinURLPath(to, req.URL)
+		if targetQuery == "" || req.URL.RawQuery == "" {
+			req.URL.RawQuery = targetQuery + req.URL.RawQuery
+		} else {
+			req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
+		}
+		if _, ok := req.Header["User-Agent"]; !ok {
+			// explicitly disable User-Agent so it's not set to default value
+			req.Header.Set("User-Agent", "")
+		}
+		//修改 localhost 记录
+		req.Host = to.Host
+
+	}
+
+	revProxy := &httputil.ReverseProxy{
+		Director:  director,
+		Transport: transport,
+	}
 	//设置连接池
-	revProxy.Transport = transport
+	//revProxy.Transport = transport
 	//异常回调
 	revProxy.ErrorHandler = httpcallback.OnError
-	//revProxy.Director = func(w *http.Request) {
-	//
-	//}
 
 	//修改回调
 	revProxy.ModifyResponse = httpcallback.ModifyResponse
@@ -129,47 +190,48 @@ type HttpHandler func(w http.ResponseWriter, r *http.Request)
 
 // Hop-by-hop headers. These are removed when sent to the backend.
 // http://www.w3.org/Protocols/rfc2616/rfc2616-sec13.html
-var hopHeaders = []string{
-	"Connection",
-	"Proxy-Connection", // non-standard but still sent by libcurl and rejected by e.g. google
-	"Keep-Alive",
-	"Proxy-Authenticate",
-	"Proxy-Authorization",
-	"Te",      // canonicalized version of "TE"
-	"Trailer", // not Trailers per URL above; http://www.rfc-editor.org/errata_search.php?eid=4522
-	"Transfer-Encoding",
-	"Upgrade",
-}
-
-func removeHeaders(header http.Header) {
-	// Remove hop-by-hop headers listed in the "Connection" header.
-	if c := header.Get("Connection"); c != "" {
-		for _, f := range strings.Split(c, ",") {
-			if f = strings.TrimSpace(f); f != "" {
-				header.Del(f)
-			}
-		}
-	}
-
-	// Remove hop-by-hop headers
-	for _, h := range hopHeaders {
-		if header.Get(h) != "" {
-			header.Del(h)
-		}
-	}
-}
-func addXForwardedForHeader(req *http.Request) {
-	if clientIP, _, err := net.SplitHostPort(req.RemoteAddr); err == nil {
-		// If we aren't the first proxy retain prior
-		// X-Forwarded-For information as a comma+space
-		// separated list and fold multiple headers into one.
-		if prior, ok := req.Header["X-Forwarded-For"]; ok {
-			clientIP = strings.Join(prior, ", ") + ", " + clientIP
-		}
-		req.Header.Set("X-Forwarded-For", clientIP)
-	}
-}
+//var hopHeaders = []string{
+//	"Connection",
+//	"Proxy-Connection", // non-standard but still sent by libcurl and rejected by e.g. google
+//	"Keep-Alive",
+//	"Proxy-Authenticate",
+//	"Proxy-Authorization",
+//	"Te",      // canonicalized version of "TE"
+//	"Trailer", // not Trailers per URL above; http://www.rfc-editor.org/errata_search.php?eid=4522
+//	"Transfer-Encoding",
+//	"Upgrade",
+//}
+//
+//func removeHeaders(header http.Header) {
+//	// Remove hop-by-hop headers listed in the "Connection" header.
+//	if c := header.Get("Connection"); c != "" {
+//		for _, f := range strings.Split(c, ",") {
+//			if f = strings.TrimSpace(f); f != "" {
+//				header.Del(f)
+//			}
+//		}
+//	}
+//
+//	// Remove hop-by-hop headers
+//	for _, h := range hopHeaders {
+//		if header.Get(h) != "" {
+//			header.Del(h)
+//		}
+//	}
+//}
+//func addXForwardedForHeader(req *http.Request) {
+//	if clientIP, _, err := net.SplitHostPort(req.RemoteAddr); err == nil {
+//		// If we aren't the first proxy retain prior
+//		// X-Forwarded-For information as a comma+space
+//		// separated list and fold multiple headers into one.
+//		if prior, ok := req.Header["X-Forwarded-For"]; ok {
+//			clientIP = strings.Join(prior, ", ") + ", " + clientIP
+//		}
+//		req.Header.Set("X-Forwarded-For", clientIP)
+//	}
+//}
 func (p *RewriteUrlMiddleWare) ProxyHTTPS(rw http.ResponseWriter, req *http.Request) {
+	req.Host = p.To.Host
 	hij, ok := rw.(http.Hijacker)
 	if !ok {
 		log.Printf("http server does not support hijacker")
@@ -225,17 +287,11 @@ func (p *RewriteUrlMiddleWare) ProxyHTTPS(rw http.ResponseWriter, req *http.Requ
 	clientConn.Close()
 }
 func (ws *RewriteUrlMiddleWare) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	var (
-		raw, old, now = strutil.String2Bytes(&r.URL.Path), strutil.String2Bytes(&ws.From.Path), strutil.String2Bytes(&ws.To.Path)
-	)
-	r.URL.Path = strutil.Byte2String(strutil.UnsafeReplaceBegin(raw, old, now))
-	addXForwardedForHeader(r)
-	removeHeaders(r.Header)
+
 	if r.Method == http.MethodConnect {
 		ws.ProxyHTTPS(w, r)
 		return
 	}
-	// connect
 
 	//直接代理转发
 	ws.Proxy.ServeHTTP(w, r)
